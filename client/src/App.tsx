81e83188df23aeb144cd7365c12903e2f1f7a9c4
officialAbhinav <official.abhinavsingh15@gmail.com>
2024-07-01 22:51:23 +0530

    project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material"
+
+
+const App = () => {
+  
+  return (
+    <div>
+      <Button variant="contained">Contained</Button>
+    </div>
+  )
+}
+
+export default App
